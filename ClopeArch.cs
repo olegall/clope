72fcbf8627d1a2e8dda218af42880a557a5b2ef4
Oleg <aleynikov.oleg@gmail.com>
2026-02-21 16:30:06 +0300

    architecture, trs cnt of clusters in phase 1, 2 differ, works slower than without architecture

---
+++ ClopeArch.cs
@@ -0,0 +1,110 @@
+﻿namespace clope;
+
+internal class ClopeArch
+{
+    private const double r = 2.6;
+    public List<ClusterArch> Clusterize(List<int[]> transactions, double r)
+    {
+        var clusters = new List<ClusterArch>();
+        AddNewCluster(clusters);
+        var cnt = 0;
+        // phase 1
+        foreach (var tr in transactions)
+        {
+            cnt++;
+            double maxCost = 0;
+            var bestChoice = 0;
+            for (var i = 0; i < clusters.Count(); i++)
+            {
+                var da = DeltaAdd(clusters[i], tr, r);
+                if (da > maxCost)
+                {
+                    maxCost = da;
+                    bestChoice = i;
+                }
+            }
+            if (clusters[bestChoice].Count == 0) AddNewCluster(clusters);
+            clusters[bestChoice].Transactions.Add(tr);
+        }
+        // phase2
+        var cnt2 = 0;
+        var moved = true;
+        while (moved)
+        {
+            moved = false;
+            foreach (var tr in transactions)
+            {
+                cnt2++;
+                double maxCost = 0;
+                var bestChoice = 0;
+                var act = clusters.FirstOrDefault(x => x.Transactions.Contains(tr)); // зафиксировать индекс кл-ра тр-и
+                var actIdx = clusters.IndexOf(act);
+                var dr = DeltaRemove(act, tr, r);
+                for (var i = 0; i < clusters.Count; i++)
+                {
+                    if (clusters[i] == act)
+                    {
+                        continue;
+                    }
+
+                    var da = DeltaAdd(clusters[i], tr, r);
+                    if (da + dr > maxCost)
+                    {
+                        maxCost = da + dr;
+                        bestChoice = i;
+                    }
+                }
+                if (maxCost > 0)
+                {
+                    if (clusters[bestChoice].Count == 0) AddNewCluster(clusters);
+                    clusters[bestChoice].Transactions.Add(tr);
+
+                    clusters[actIdx].Transactions.Remove(tr);
+                    clusters[bestChoice].Transactions.Add(tr);
+                    moved = true;
+                }
+            }
+        }
+        // TODO удалить пустые кластеры
+        return clusters;
+    }
+
+    private double DeltaAdd(ClusterArch C, IEnumerable<int> t, double r)
+    {
+        if (C.Count == 0) return t.Count() / Math.Pow(t.Count(), r);
+        var Snew = C.S + t.Count();
+        var Wnew = C.W;
+        var hg = C.Histogram;
+        for (int i = 0; i < t.Count() - 1; i++)
+        {
+            if (!hg.ContainsKey(t.ElementAt(i))) // occ
+            {
+                Wnew += 1;
+            }
+        }
+        var result = Snew * (C.N + 1) / Wnew.P(r) - C.S * C.N / C.W.P(r);
+        return result;
+    }
+
+    private double DeltaRemove(ClusterArch C, IEnumerable<int> t, double r)
+    {
+        var Snew = C.S - t.Count();
+        var Wnew = C.W;
+        var hg = C.Histogram;
+        for (int i = 0; i < t.Count() - 1; i++)
+        {
+            if (!hg.ContainsKey(t.ElementAt(i)))
+            {
+                Wnew -= 1;
+            }
+        }
+        var result = Snew * (C.N - 1) / Wnew.P(r) - C.S * C.N / C.W.P(r);
+        return result;
+    }
+
+    private void AddNewCluster(List<ClusterArch> clusters) // TODO AddNew класса Cluster
+    {
+        var cluster = new ClusterArch();
+        clusters.Add(cluster);
+    }
+}
---
+++ Cluster.cs
@@ -0,0 +1,83 @@
+﻿namespace clope;
+
+internal class Cluster
+{
+    private readonly List<IEnumerable<int>> _C;
+    //public Cluster(List<IEnumerable<int>> C)
+    //{
+    //    _C = C;
+    //}
+
+    public List<IEnumerable<int>> Transactions { get; set; }
+
+    public Dictionary<int, int> Histogram
+    {
+        get
+        {
+            var histogram = Trs.GroupBy(x => x); //var histogram = trs.GroupBy(x => x).ToDictionary<int, double>();
+            var histogram_ = new Dictionary<int, int>(); // <TOcc, TObject>
+            foreach (var item in histogram.ToArray()) histogram_.Add(item.Key, item.Count()); //histogram.Select(x => histogram_.Add(x.Key, x.Count()));
+            return histogram_;
+        }
+    }
+
+    public int Count => _C.Count();
+    public int S => Trs.Count();
+    public int W => Histogram.Count();
+
+    public int N => _C.Count();
+
+    private IEnumerable<int> Trs => _C.SelectMany(x => x);
+}
+
+internal class ClusterArch
+{
+    public List<IEnumerable<int>> Transactions { get; set; } = new List<IEnumerable<int>>();
+
+    public Dictionary<int, int> Histogram
+    {
+        get
+        {
+            var histogram = Trs.GroupBy(x => x); //var histogram = trs.GroupBy(x => x).ToDictionary<int, double>();
+            var histogram_ = new Dictionary<int, int>(); // <TOcc, TObject>
+            foreach (var item in histogram.ToArray()) histogram_.Add(item.Key, item.Count()); //histogram.Select(x => histogram_.Add(x.Key, x.Count()));
+            return histogram_;
+        }
+    }
+
+    public int Count => Transactions.Count();
+    public int S => Trs.Count();
+    public int W => Histogram.Count();
+
+    public int N => Transactions.Count();
+
+    private IEnumerable<int> Trs => Transactions.SelectMany(x => x);
+}
+
+internal static class C
+{
+    public static List<IEnumerable<int>> self;
+    //static C_(List<IEnumerable<int>> C)
+    //{
+    //    _C = C;
+    //}
+
+    public static Dictionary<int, int> Histogram
+    {
+        get
+        {
+            var histogram = Trs.GroupBy(x => x); //var histogram = trs.GroupBy(x => x).ToDictionary<int, double>();
+            var histogram_ = new Dictionary<int, int>(); // <TOcc, TObject>
+            foreach (var item in histogram.ToArray()) histogram_.Add(item.Key, item.Count()); //histogram.Select(x => histogram_.Add(x.Key, x.Count()));
+            return histogram_;
+        }
+    }
+
+    public static int S => Trs.Count();
+
+    public static int W => Histogram.Count();
+
+    public static int N => self.Count();
+
+    private static IEnumerable<int> Trs => self.SelectMany(x => x);
+}
